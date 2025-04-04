aeb845f142d4e270df45e25783f255a1dd928065
Nqla <151285370+EthanCarreon@users.noreply.github.com>
2025-04-04 14:08:37 -0400

    importation

---
+++ Assets/Scripts/AT_PatrolTwo.cs
@@ -0,0 +1,51 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace NodeCanvas.Tasks.Actions
+{
+    public class AT_PatrolTwo : ActionTask
+    {
+        public BBParameter<List<Transform>> patrolPoints;
+        public BBParameter<int> maxPatrolRounds;
+        public BBParameter<float> speed = 3f;
+        public float arrivalDistance = 1f;
+
+        public BBParameter<int> patrolRounds = 0;
+        public int currentPatrolPointIndex = 0;
+
+        protected override void OnExecute()
+        {
+            currentPatrolPointIndex = 0;
+        }
+
+        protected override void OnUpdate()
+        {
+            Transform targetPatrolPoint = patrolPoints.value[currentPatrolPointIndex];
+
+            float distanceToTarget = Vector3.Distance(agent.transform.position, targetPatrolPoint.position);
+
+            if (distanceToTarget < arrivalDistance)
+            {
+                currentPatrolPointIndex = (currentPatrolPointIndex + 1);
+
+                if (currentPatrolPointIndex >= patrolPoints.value.Count)
+                {
+                    currentPatrolPointIndex = 0;
+                    patrolRounds.value++;
+                }
+
+                targetPatrolPoint = patrolPoints.value[currentPatrolPointIndex];
+            }
+
+            Vector3 moveDirection = (targetPatrolPoint.position - agent.transform.position).normalized;
+            agent.transform.position += moveDirection * speed.value * Time.deltaTime;
+
+            if (patrolRounds.value >= maxPatrolRounds.value)
+            {
+                EndAction(true);
+            }
+        }
+    }
+}
